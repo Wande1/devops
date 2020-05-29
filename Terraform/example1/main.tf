98b0c088efecf0e667b8787daac9222c356c5d5d
Yewande <47472903+Wande1@users.noreply.github.com>
2020-05-29 07:54:22 -0600

    Update main.tf

--- Terraform/example1/main.tf
+++ Terraform/example1/main.tf
@@ -3,16 +3,16 @@
 ##
 ################################
 provider "aws" {
-  region                  = "us-east-1"
+  region                  = "us-east-2"
   profile                 = "default"
 }
 
 resource "aws_instance" "titotesting" {
-    ami                   = "ami-039a49e70ea773ffc"
+    ami                   = "ami-0a54aef4ef3b5f881"
     instance_type         = "t2.micro"
-    key_name              = "bees-tito-profile" #USE YOUR PROFILE KEY HERE
+    key_name              = "wendy-me" #USE YOUR PROFILE KEY HERE
     tags = {
-        Name = "The first server"
+        Name = "Wendy server"
         Env  = "Development"
     }
 }
