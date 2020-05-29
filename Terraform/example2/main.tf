fed4680fdab0da624cdada8a7d473f5315cee14e
Yewande <47472903+Wande1@users.noreply.github.com>
2020-05-29 07:57:09 -0600

    Update main.tf

--- Terraform/example2/main.tf
+++ Terraform/example2/main.tf
@@ -3,20 +3,20 @@
 ## Attach elastic IP Address
 ################################
 provider "aws" {
-  region                  = "us-east-1"
+  region                  = "us-east-2"
   profile                 = "default"
 }
 
 resource "aws_instance" "titotesting" {
-    ami                   = "ami-039a49e70ea773ffc"
+    ami                   = "ami-0a54aef4ef3b5f881"
     instance_type         = "t2.micro"
-    key_name              = "bees-tito-profile" #USE YOUR PROFILE KEY HERE
+    key_name              = "wendy-me" #USE YOUR PROFILE KEY HERE
     tags = {
-        Name = "The first server"
+        Name = "Wendy server"
         Env  = "Development"
     }
 }
 ## Attach elastic IP to the ec2instance created above
 resource "aws_eip" "ip"{
-    instance    = aws_instance.titotesting.id
+    instance    = wande
 }
