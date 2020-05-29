32aaa3139b1f90c12ae1dee91d2606a7797f0df9
Yewande <47472903+Wande1@users.noreply.github.com>
2020-05-29 08:11:24 -0600

    Update main.tf

--- Terraform/example3/main.tf
+++ Terraform/example3/main.tf
@@ -11,9 +11,9 @@ resource "aws_instance" "titotesting" {
     ami                   = "ami-ami-0a54aef4ef3b5f881"
     count                 = 2
     instance_type         = "t2.micro"
-    key_name              = "wendy-me" #USE YOUR PROFILE KEY HERE
+    key_name              = "wendy_me" #USE YOUR PROFILE KEY HERE
     tags = {
-        Name = "Wendy Server"
+        Name = "wande"
         Env  = "Development"
     }
 }
@@ -21,6 +21,6 @@ resource "aws_instance" "titotesting" {
 ## Attach elastic IP to the ec2instance created above
 resource "aws_eip" "ip"{
     count   = 2
-    instance = element(aws_instance.Wendy Server.*.id, count.index)
+    instance = wande(aws_instance.Wendy Server.*.id, count.index)
 }
 
