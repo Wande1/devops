86cab09793366609d0b9e8d34aed14d9a6986068
Yewande <47472903+Wande1@users.noreply.github.com>
2020-05-29 08:09:27 -0600

    Update main.tf

--- Terraform/example1/main.tf
+++ Terraform/example1/main.tf
@@ -10,9 +10,9 @@ provider "aws" {
 resource "aws_instance" "titotesting" {
     ami                   = "ami-0a54aef4ef3b5f881"
     instance_type         = "t2.micro"
-    key_name              = "wendy-me" #USE YOUR PROFILE KEY HERE
+    key_name              = "wendy_me" #USE YOUR PROFILE KEY HERE
     tags = {
-        Name = "Wendy server"
+        Name = "Wendyme"
         Env  = "Development"
     }
 }
